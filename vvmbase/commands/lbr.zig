644d440d5c70093427ed3355a54bf5f73dec57f8
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-07 19:48:15 +0100

    Improved test coverage

--- vvmbase/commands/lbr.zig
+++ vvmbase/commands/lbr.zig
@@ -14,11 +14,13 @@ pub fn handler(comptime command_code: u8) Handler {
 test "Test" {
     var vvm: Vvm = undefined;
 
-    vvm.memory[0] = 0; // lbr b0
-    vvm.registers.gp.b[0] = 0x10;
-    vvm.registers.a.w = 0;
-    vvm.registers.pc = 0;
-    vvm.step();
+    for (0..7) |index| {
+        vvm.memory[0] = @intCast(0x00 + index); // lbr b0
+        vvm.registers.gp.b[index] = 0x10;
+        vvm.registers.a.w = 0;
+        vvm.registers.pc = 0;
+        vvm.step();
 
-    try std.testing.expectEqual(0x10, vvm.registers.a.b[0]);
+        try std.testing.expectEqual(0x10, vvm.registers.a.b[0]);
+    }
 }
--- vvmbase/commands/lwr.zig
+++ vvmbase/commands/lwr.zig
@@ -14,11 +14,13 @@ pub fn handler(comptime command_code: u8) Handler {
 test "Test" {
     var vvm: Vvm = undefined;
 
-    vvm.memory[0] = 8; // lwr w0
-    vvm.registers.gp.w[0] = 0x1110;
-    vvm.registers.a.w = 0;
-    vvm.registers.pc = 0;
-    vvm.step();
+    for (0..3) |index| {
+        vvm.memory[0] = @intCast(0x08 + index); // lwr w0
+        vvm.registers.gp.w[index] = 0x1110;
+        vvm.registers.a.w = 0;
+        vvm.registers.pc = 0;
+        vvm.step();
 
-    try std.testing.expectEqual(0x1110, vvm.registers.a.w);
+        try std.testing.expectEqual(0x1110, vvm.registers.a.w);
+    }
 }
