59e068df6097618f29ec61cc84936c8783f0d08a
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-07 22:52:30 +0100

    Added CPL command

--- vvmbase/commands.zig
+++ vvmbase/commands.zig
@@ -96,5 +96,6 @@ const commands = struct {
     pub const arwr = @import("commands/arwr.zig");
     pub const zero = @import("commands/zero.zig");
     pub const all = @import("commands/all.zig");
+    pub const cpl = @import("commands/cpl.zig");
     pub const nop = @import("commands/nop.zig");
 };
--- vvmbase/commands/cpl.zig
+++ vvmbase/commands/cpl.zig
@@ -2,19 +2,19 @@ const std = @import("std");
 const Vvm = @import("../Vvm.zig");
 const commands = @import("../commands.zig");
 
-pub const descriptor = commands.Descriptor.init(0x51);
+pub const descriptor = commands.Descriptor.init(0x52);
 
 pub fn handler(vvm: *Vvm) void {
-    vvm.registers.a.w = 0xFFFF;
+    vvm.registers.a.w = ~vvm.registers.a.w;
 }
 
 test "Test" {
     var vvm: Vvm = undefined;
 
-    vvm.memory[0] = @intCast(descriptor.base); // ALL
+    vvm.memory[0] = @intCast(descriptor.base); // CPL
     vvm.registers.a.w = 0x9110;
     vvm.registers.pc = 0;
     vvm.step();
 
-    try std.testing.expectEqual(0xFFFF, vvm.registers.a.w);
+    try std.testing.expectEqual(0x9110 ^ 0xFFFF, vvm.registers.a.w);
 }
