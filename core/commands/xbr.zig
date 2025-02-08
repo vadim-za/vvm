a19aa69ae7b203ef89e316925b305d8f28f5bcea
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 10:46:01 +0100

    WIP: refactoring command system - XBR

--- core/commands/xbr.zig
+++ core/commands/xbr.zig
@@ -1,13 +1,8 @@
 const std = @import("std");
 const Vvm = @import("../Vvm.zig");
-const commands = @import("../commands.zig");
+const Command = @import("../Command.zig");
 
-pub const descriptor = commands.Descriptor.initRange(
-    0x20,
-    8,
-);
-
-pub fn handler(comptime command_code: u8) commands.Handler {
+pub fn handler(comptime command_code: u8) Command.Handler {
     return struct {
         fn actualHandler(vvm: *Vvm) void {
             const index: u3 = command_code & 7;
@@ -17,18 +12,20 @@ pub fn handler(comptime command_code: u8) commands.Handler {
 }
 
 test "Test" {
+    const xbr = Command.collection.xbr;
     var vvm: Vvm = undefined;
 
-    for (0..descriptor.count) |n| {
-        const value_offs: u8 = @intCast(n);
+    for (0..xbr.variant_count) |n| {
+        const value_offs8: u8 = @intCast(n);
+        const value_offs16: u16 = value_offs8;
 
-        vvm.memory[0] = @intCast(descriptor.base + n); // XBR Bn
-        vvm.registers.gp.b[n] = 0x10 + value_offs;
-        vvm.registers.a.w = 0xA090 + @as(u16, value_offs);
+        vvm.memory[0] = @intCast(xbr.base_code + n); // XBR Bn
+        vvm.registers.gp.b[n] = 0x10 + value_offs8;
+        vvm.registers.a.w[0] = 0xA090 + value_offs16;
         vvm.registers.pc = 0;
         vvm.step();
 
-        try std.testing.expectEqual(0x10 + value_offs, vvm.registers.a.b[0]);
-        try std.testing.expectEqual(0x90 + value_offs, vvm.registers.gp.b[n]);
+        try std.testing.expectEqual(0xA010 + value_offs16, vvm.registers.a.w[0]);
+        try std.testing.expectEqual(0x90 + value_offs8, vvm.registers.gp.b[n]);
     }
 }
