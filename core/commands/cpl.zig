4d7d31a04930387345cf2e8827ed2a60088f9a36
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 13:55:05 +0100

    WIP: refactoring command system - CPL

--- core/commands/cpl.zig
+++ core/commands/cpl.zig
@@ -1,20 +1,19 @@
 const std = @import("std");
 const Vvm = @import("../Vvm.zig");
-const commands = @import("../commands.zig");
-
-pub const descriptor = commands.Descriptor.init(0x52);
+const Command = @import("../Command.zig");
 
 pub fn handler(vvm: *Vvm) void {
-    vvm.registers.a.w = ~vvm.registers.a.w;
+    vvm.registers.a.w[0] = ~vvm.registers.a.w[0];
 }
 
 test "Test" {
+    const cpl = Command.collection.cpl;
     var vvm: Vvm = undefined;
 
-    vvm.memory[0] = @intCast(descriptor.base); // CPL
-    vvm.registers.a.w = 0x9110;
+    vvm.memory[0] = cpl.code(0); // CPL
+    vvm.registers.a.w[0] = 0x9110;
     vvm.registers.pc = 0;
     vvm.step();
 
-    try std.testing.expectEqual(0x9110 ^ 0xFFFF, vvm.registers.a.w);
+    try std.testing.expectEqual(0x9110 ^ 0xFFFF, vvm.registers.a.w[0]);
 }
