6e176750fef5c3401cc6dc50caa7798b6ef73a82
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 20:08:13 +0100

    JIF

---
+++ core/commands/jif.zig
@@ -0,0 +1,140 @@
+const std = @import("std");
+const Vvm = @import("../Vvm.zig");
+const Command = @import("../Command.zig");
+
+pub fn handler(comptime command_code: u8) Command.Handler {
+    return struct {
+        fn actualHandler(vvm: *Vvm) void {
+            const index: u3 = command_code & 7;
+            if (condition(vvm, index))
+                vvm.registers.pc = vvm.registers.addr;
+        }
+    }.actualHandler;
+}
+
+fn condition(vvm: *Vvm, comptime condition_index: u3) bool {
+    const bit0: u1 = condition_index & 1;
+    const bit1: u1 = condition_index >> 1 & 1;
+    const bit2: u1 = condition_index >> 2 & 1;
+
+    const value = if (comptime bit2 != 0)
+        vvm.registers.a.w[bit1]
+    else
+        vvm.registers.a.b[bit1];
+
+    return if (comptime bit0 != 0)
+        value != 0
+    else
+        value == 0;
+}
+
+test "Test" {
+    const jif = Command.collection.jif;
+
+    var vvm: Vvm = undefined;
+    vvm.init();
+
+    vvm.memory[0] = jif.codeVariant(0); // JIFLZ
+    vvm.registers.addr = 0x1002;
+
+    vvm.registers.a.dw = 0xFFFF_FF00;
+    vvm.registers.pc = 0;
+    vvm.step();
+    try std.testing.expectEqual(0x1002, vvm.registers.pc); // taken
+
+    vvm.registers.a.dw = 0xFF;
+    vvm.registers.pc = 0;
+    vvm.step();
+    try std.testing.expectEqual(1, vvm.registers.pc); // not taken
+
+    vvm.memory[0] = jif.codeVariant(1); // JIFLNZ
+    vvm.registers.addr = 0x1002;
+
+    vvm.registers.a.dw = 0xFF;
+    vvm.registers.pc = 0;
+    vvm.step();
+    try std.testing.expectEqual(0x1002, vvm.registers.pc); // taken
+
+    vvm.registers.a.dw = 0xFFFF_FF00;
+    vvm.registers.pc = 0;
+    vvm.step();
+    try std.testing.expectEqual(1, vvm.registers.pc); // not taken
+
+    vvm.memory[0] = jif.codeVariant(2); // JIFHZ
+    vvm.registers.addr = 0x1002;
+
+    vvm.registers.a.dw = 0xFFFF_00FF;
+    vvm.registers.pc = 0;
+    vvm.step();
+    try std.testing.expectEqual(0x1002, vvm.registers.pc); // taken
+
+    vvm.registers.a.dw = 0xFF00;
+    vvm.registers.pc = 0;
+    vvm.step();
+    try std.testing.expectEqual(1, vvm.registers.pc); // not taken
+
+    vvm.memory[0] = jif.codeVariant(3); // JIFHNZ
+    vvm.registers.addr = 0x1002;
+
+    vvm.registers.a.dw = 0xFF00;
+    vvm.registers.pc = 0;
+    vvm.step();
+    try std.testing.expectEqual(0x1002, vvm.registers.pc); // taken
+
+    vvm.registers.a.dw = 0xFFFF_00FF;
+    vvm.registers.pc = 0;
+    vvm.step();
+    try std.testing.expectEqual(1, vvm.registers.pc); // not taken
+
+    vvm.memory[0] = jif.codeVariant(4); // JIFZ
+    vvm.registers.addr = 0x1002;
+
+    vvm.registers.a.dw = 0xFFFF_0000;
+    vvm.registers.pc = 0;
+    vvm.step();
+    try std.testing.expectEqual(0x1002, vvm.registers.pc); // taken
+
+    vvm.registers.a.dw = 0xFFFF;
+    vvm.registers.pc = 0;
+    vvm.step();
+    try std.testing.expectEqual(1, vvm.registers.pc); // not taken
+
+    vvm.memory[0] = jif.codeVariant(5); // JIFNZ
+    vvm.registers.addr = 0x1002;
+
+    vvm.registers.a.dw = 0xFFFF;
+    vvm.registers.pc = 0;
+    vvm.step();
+    try std.testing.expectEqual(0x1002, vvm.registers.pc); // taken
+
+    vvm.registers.a.dw = 0xFFFF_0000;
+    vvm.registers.pc = 0;
+    vvm.step();
+    try std.testing.expectEqual(1, vvm.registers.pc); // not taken
+
+    vvm.memory[0] = jif.codeVariant(6); // JIFXZ
+    vvm.registers.addr = 0x1002;
+
+    vvm.registers.a.dw = 0xFFFF;
+    vvm.registers.pc = 0;
+    vvm.step();
+    try std.testing.expectEqual(0x1002, vvm.registers.pc); // taken
+
+    vvm.registers.a.dw = 0xFFFF_0000;
+    vvm.registers.pc = 0;
+    vvm.step();
+    try std.testing.expectEqual(1, vvm.registers.pc); // not taken
+
+    vvm.memory[0] = jif.codeVariant(7); // JIFXNZ
+    vvm.registers.addr = 0x1002;
+
+    vvm.registers.a.dw = 0xFFFF_0000;
+    vvm.registers.pc = 0;
+    vvm.step();
+    try std.testing.expectEqual(0x1002, vvm.registers.pc); // taken
+
+    vvm.registers.a.dw = 0xFFFF;
+    vvm.registers.pc = 0;
+    vvm.step();
+    try std.testing.expectEqual(1, vvm.registers.pc); // not taken
+}
