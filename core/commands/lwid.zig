8b38ebabd34299b2c0b99194a04d87201c22ed67
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 18:03:59 +0100

    WIP: refactoring command system - LWID

---
+++ core/commands/lwid.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+const Vvm = @import("../Vvm.zig");
+const Command = @import("../Command.zig");
+
+pub fn handler(vvm: *Vvm) void {
+    const displacement = vvm.fetchCommandWord();
+    vvm.registers.a.w[0] = vvm.readMemoryWord(vvm.registers.addr +% displacement);
+}
+
+test "Test" {
+    const lwid = Command.collection.lwid;
+
+    var vvm: Vvm = undefined;
+    vvm.init();
+
+    @memcpy(vvm.memory[0..3], &lwid.codeWithLiteral16(0xEFFE)); // LWID disp
+    vvm.memory[0x1002] = 0x34;
+    vvm.memory[0x1003] = 0x12;
+    vvm.registers.a.dw = 0;
+    vvm.registers.addr = 0x2004;
+    vvm.registers.pc = 0;
+    vvm.step();
+
+    try std.testing.expectEqual(0x1234, vvm.registers.a.w[0]);
+}
