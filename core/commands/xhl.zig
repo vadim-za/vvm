de0bceeace49f0438d69f59afc21ef4cd8c5dee0
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 13:56:07 +0100

    WIP: refactoring command system - XHL

--- core/commands/xhl.zig
+++ core/commands/xhl.zig
@@ -1,20 +1,19 @@
 const std = @import("std");
 const Vvm = @import("../Vvm.zig");
-const commands = @import("../commands.zig");
-
-pub const descriptor = commands.Descriptor.init(0x53);
+const Command = @import("../Command.zig");
 
 pub fn handler(vvm: *Vvm) void {
     std.mem.swap(u8, &vvm.registers.a.b[0], &vvm.registers.a.b[1]);
 }
 
 test "Test" {
+    const xhl = Command.collection.xhl;
     var vvm: Vvm = undefined;
 
-    vvm.memory[0] = @intCast(descriptor.base); // XHL
-    vvm.registers.a.w = 0x9110;
+    vvm.memory[0] = xhl.code(0); // XHL
+    vvm.registers.a.w[0] = 0x9110;
     vvm.registers.pc = 0;
     vvm.step();
 
-    try std.testing.expectEqual(0x1091, vvm.registers.a.w);
+    try std.testing.expectEqual(0x1091, vvm.registers.a.w[0]);
 }
