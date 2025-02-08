44d58c728feadbf7881d91065b9305c412cc5851
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 10:36:42 +0100

    WIP: refactoring command system - STWR

--- core/commands/stwr.zig
+++ core/commands/stwr.zig
@@ -1,30 +1,26 @@
 const std = @import("std");
 const Vvm = @import("../Vvm.zig");
-const commands = @import("../commands.zig");
+const Command = @import("../Command.zig");
 
-pub const descriptor = commands.Descriptor.initRange(
-    0x18,
-    4,
-);
-
-pub fn handler(comptime command_code: u8) commands.Handler {
+pub fn handler(comptime command_code: u8) Command.Handler {
     return struct {
         fn actualHandler(vvm: *Vvm) void {
             const index: u2 = command_code & 3;
-            vvm.registers.gp.w[index] = vvm.registers.a.w;
+            vvm.registers.gp.w[index] = vvm.registers.a.w[0];
         }
     }.actualHandler;
 }
 
 test "Test" {
+    const stwr = Command.collection.stwr;
     var vvm: Vvm = undefined;
 
-    for (0..descriptor.count) |n| {
+    for (0..stwr.variant_count) |n| {
         const value16: u16 = 0x9110 + @as(u16, @intCast(n));
 
-        vvm.memory[0] = @intCast(descriptor.base + n); // STWR Wn
+        vvm.memory[0] = @intCast(stwr.base_code + n); // STWR Wn
         vvm.registers.gp.w[n] = 0;
-        vvm.registers.a.w = value16;
+        vvm.registers.a.w[0] = value16;
         vvm.registers.pc = 0;
         vvm.step();
 
