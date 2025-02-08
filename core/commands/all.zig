dc455d6df2d0cb357d759e33f3386d1c02f95a5a
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 11:37:14 +0100

    WIP: refactoring command system - ALL

--- core/commands/all.zig
+++ core/commands/all.zig
@@ -1,20 +1,19 @@
 const std = @import("std");
 const Vvm = @import("../Vvm.zig");
-const commands = @import("../commands.zig");
-
-pub const descriptor = commands.Descriptor.init(0x51);
+const Command = @import("../Command.zig");
 
 pub fn handler(vvm: *Vvm) void {
-    vvm.registers.a.w = 0xFFFF;
+    vvm.registers.a.w[0] = 0xFFFF;
 }
 
 test "Test" {
+    const all = Command.collection.all;
     var vvm: Vvm = undefined;
 
-    vvm.memory[0] = @intCast(descriptor.base); // ALL
-    vvm.registers.a.w = 0x9110;
+    vvm.memory[0] = all.code(0); // ALL
+    vvm.registers.a.w[0] = 0x9110;
     vvm.registers.pc = 0;
     vvm.step();
 
-    try std.testing.expectEqual(0xFFFF, vvm.registers.a.w);
+    try std.testing.expectEqual(0xFFFF, vvm.registers.a.w[0]);
 }
