a21df5f2a154deca62f40abed9962013501f0924
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 11:21:05 +0100

    WIP: refactoring command system - ZERO

--- core/commands/zero.zig
+++ core/commands/zero.zig
@@ -1,20 +1,19 @@
 const std = @import("std");
 const Vvm = @import("../Vvm.zig");
-const commands = @import("../commands.zig");
-
-pub const descriptor = commands.Descriptor.init(0x50);
+const Command = @import("../Command.zig");
 
 pub fn handler(vvm: *Vvm) void {
-    vvm.registers.a.w = 0;
+    vvm.registers.a.w[0] = 0;
 }
 
 test "Test" {
+    const zero = Command.collection.zero;
     var vvm: Vvm = undefined;
 
-    vvm.memory[0] = @intCast(descriptor.base); // ZERO
-    vvm.registers.a.w = 0x9110;
+    vvm.memory[0] = zero.code(0); // ZERO
+    vvm.registers.a.w[0] = 0x9110;
     vvm.registers.pc = 0;
     vvm.step();
 
-    try std.testing.expectEqual(0, vvm.registers.a.w);
+    try std.testing.expectEqual(0, vvm.registers.a.w[0]);
 }
