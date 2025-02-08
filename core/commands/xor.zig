1bc2539c99315ca0816ecb05ad465987f2dce262
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 11:17:44 +0100

    WIP: refactoring command system - XOR

--- core/commands/xor.zig
+++ core/commands/xor.zig
@@ -1,21 +1,20 @@
 const std = @import("std");
 const Vvm = @import("../Vvm.zig");
-const commands = @import("../commands.zig");
-
-pub const descriptor = commands.Descriptor.init(0x44);
+const Command = @import("../Command.zig");
 
 pub fn handler(vvm: *Vvm) void {
-    vvm.registers.a.w ^= vvm.registers.x;
+    vvm.registers.a.w[0] ^= vvm.registers.a.w[1];
 }
 
 test "Test" {
+    const xor = Command.collection.xor;
     var vvm: Vvm = undefined;
 
-    vvm.memory[0] = @intCast(descriptor.base); // XOR
-    vvm.registers.a.w = 0x9112;
-    vvm.registers.x = 0xC00E;
+    vvm.memory[0] = xor.code(0); // XOR
+    vvm.registers.a.w[0] = 0x9112;
+    vvm.registers.a.w[1] = 0xC00E;
     vvm.registers.pc = 0;
     vvm.step();
 
-    try std.testing.expectEqual(0x511C, vvm.registers.a.w);
+    try std.testing.expectEqual(0x511C, vvm.registers.a.w[0]);
 }
