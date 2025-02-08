2c16603a002252e7fd81db220512784ac7859775
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 19:45:53 +0100

    RET

---
+++ core/commands/ret.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+const Vvm = @import("../Vvm.zig");
+const Command = @import("../Command.zig");
+
+pub fn handler(vvm: *Vvm) void {
+    vvm.registers.pc = vvm.popWord();
+}
+
+test "Test" {
+    const ret = Command.collection.ret;
+
+    var vvm: Vvm = undefined;
+    vvm.init();
+
+    vvm.memory[0x5678] = ret.code(); // RET
+    vvm.memory[0xEFFE] = 0x34;
+    vvm.memory[0xEFFF] = 0x12;
+    vvm.registers.addr = 0xFFFF;
+    vvm.registers.sp = 0xEFFE;
+    vvm.registers.pc = 0x5678;
+    vvm.step();
+
+    try std.testing.expectEqual(0x1234, vvm.registers.pc);
+    try std.testing.expectEqual(0xF000, vvm.registers.sp);
+}
