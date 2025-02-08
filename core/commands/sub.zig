7b73ea109baa196939c46c87c93484f7823521e1
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 10:53:14 +0100

    WIP: refactoring command system - SUB

--- core/commands/sub.zig
+++ core/commands/sub.zig
@@ -1,24 +1,21 @@
 const std = @import("std");
 const Vvm = @import("../Vvm.zig");
-const commands = @import("../commands.zig");
-
-pub const descriptor = commands.Descriptor.init(0x41);
+const Command = @import("../Command.zig");
 
 pub fn handler(vvm: *Vvm) void {
-    const result: u32 = @as(u32, vvm.registers.a.w) -% @as(u32, vvm.registers.x);
-    vvm.registers.a.w = @truncate(result);
-    vvm.registers.x = @intCast(result >> 16);
+    vvm.registers.a.dw = @as(u32, vvm.registers.a.w[0]) -% @as(u32, vvm.registers.a.w[1]);
 }
 
 test "Test" {
+    const sub = Command.collection.sub;
     var vvm: Vvm = undefined;
 
-    vvm.memory[0] = @intCast(descriptor.base); // SUB
-    vvm.registers.a.w = 0x9110;
-    vvm.registers.x = 0xB000;
+    vvm.memory[0] = @intCast(sub.base_code); // SUB
+    vvm.registers.a.w[0] = 0x9110;
+    vvm.registers.a.w[1] = 0xB000;
     vvm.registers.pc = 0;
     vvm.step();
 
-    try std.testing.expectEqual(0xE110, vvm.registers.a.w);
-    try std.testing.expectEqual(0xFFFF, vvm.registers.x);
+    try std.testing.expectEqual(0xE110, vvm.registers.a.w[0]);
+    try std.testing.expectEqual(0xFFFF, vvm.registers.a.w[1]);
 }
