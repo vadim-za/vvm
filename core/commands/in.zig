bcd81e65bbaf8b1cd6028cc04660af7d6f7c301d
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 19:26:52 +0100

    WIP: refactoring command system - IN

---
+++ core/commands/in.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+const Vvm = @import("../Vvm.zig");
+const Command = @import("../Command.zig");
+
+pub fn handler(vvm: *Vvm) void {
+    const port: u8 = @truncate(vvm.registers.addr);
+    vvm.registers.a.b[0] = vvm.env.vft.in(vvm.env.ptr, port);
+}
+
+test "Test" {
+    const Env = struct {
+        vvm: *Vvm,
+        offs: u8,
+        pub fn in(ptr: ?*anyopaque, port: u8) u8 {
+            const self: *@This() = @alignCast(@ptrCast(ptr.?));
+            return port +% self.offs;
+        }
+        pub fn out(_: ?*anyopaque, _: u8, _: u8) void {}
+    };
+
+    const in = Command.collection.in;
+
+    var vvm: Vvm = undefined;
+    var env = Env{ .vvm = &vvm, .offs = 0xF0 };
+    vvm.init();
+    vvm.env = .init(Env, &env);
+
+    vvm.memory[0] = in.code(); // LBI
+    vvm.registers.a.dw = 0;
+    vvm.registers.addr = 0x1234;
+    vvm.registers.pc = 0;
+    vvm.step();
+
+    try std.testing.expectEqual(0x24, vvm.registers.a.b[0]);
+}
