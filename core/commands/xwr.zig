1344112d3eb4ac28c8667e7f9ff25d03c027b515
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 10:49:09 +0100

    WIP: refactoring command system - XWR

--- core/commands/xwr.zig
+++ core/commands/xwr.zig
@@ -1,34 +1,30 @@
 const std = @import("std");
 const Vvm = @import("../Vvm.zig");
-const commands = @import("../commands.zig");
+const Command = @import("../Command.zig");
 
-pub const descriptor = commands.Descriptor.initRange(
-    0x28,
-    4,
-);
-
-pub fn handler(comptime command_code: u8) commands.Handler {
+pub fn handler(comptime command_code: u8) Command.Handler {
     return struct {
         fn actualHandler(vvm: *Vvm) void {
             const index: u2 = command_code & 3;
-            std.mem.swap(u16, &vvm.registers.a.w, &vvm.registers.gp.w[index]);
+            std.mem.swap(u16, &vvm.registers.a.w[0], &vvm.registers.gp.w[index]);
         }
     }.actualHandler;
 }
 
 test "Test" {
+    const xwr = Command.collection.xwr;
     var vvm: Vvm = undefined;
 
-    for (0..descriptor.count) |n| {
+    for (0..xwr.variant_count) |n| {
         const value_offs: u16 = @intCast(n);
 
-        vvm.memory[0] = @intCast(descriptor.base + n); // LWR Wn
+        vvm.memory[0] = @intCast(xwr.base_code + n); // XWR Wn
         vvm.registers.gp.w[n] = 0xC010 + value_offs;
-        vvm.registers.a.w = 0xA090 + value_offs;
+        vvm.registers.a.w[0] = 0xA090 + value_offs;
         vvm.registers.pc = 0;
         vvm.step();
 
-        try std.testing.expectEqual(0xC010 + value_offs, vvm.registers.a.w);
+        try std.testing.expectEqual(0xC010 + value_offs, vvm.registers.a.w[0]);
         try std.testing.expectEqual(0xA090 + value_offs, vvm.registers.gp.w[n]);
     }
 }
