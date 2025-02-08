b16679c25565d0b8845749a4e962580591c76ce4
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 14:31:24 +0100

    WIP: refactoring command system - XA

--- core/commands/xa.zig
+++ core/commands/xa.zig
@@ -1,22 +1,21 @@
 const std = @import("std");
 const Vvm = @import("../Vvm.zig");
-const commands = @import("../commands.zig");
-
-pub const descriptor = commands.Descriptor.init(0x5B);
+const Command = @import("../Command.zig");
 
 pub fn handler(vvm: *Vvm) void {
-    std.mem.swap(u16, &vvm.registers.a.w, &vvm.registers.x);
+    std.mem.swap(u16, &vvm.registers.a.w[0], &vvm.registers.a.w[1]);
 }
 
 test "Test" {
+    const xa = Command.collection.xa;
     var vvm: Vvm = undefined;
 
-    vvm.memory[0] = @intCast(descriptor.base); // XA
-    vvm.registers.a.w = 0xFED9;
-    vvm.registers.x = 0xBA98;
+    vvm.memory[0] = xa.code(); // XA
+    vvm.registers.a.w[0] = 0xFED9;
+    vvm.registers.a.w[1] = 0xBA98;
     vvm.registers.pc = 0;
     vvm.step();
 
-    try std.testing.expectEqual(0xBA98, vvm.registers.a.w);
-    try std.testing.expectEqual(0xFED9, vvm.registers.x);
+    try std.testing.expectEqual(0xBA98, vvm.registers.a.w[0]);
+    try std.testing.expectEqual(0xFED9, vvm.registers.a.w[1]);
 }
