2a2223bbac6853735c7d4338b161b43f2d11bbb6
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 18:08:56 +0100

    WIP: refactoring command system - STSP

---
+++ core/commands/stsp.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+const Vvm = @import("../Vvm.zig");
+const Command = @import("../Command.zig");
+
+pub fn handler(vvm: *Vvm) void {
+    vvm.registers.sp = vvm.registers.a.w[0];
+}
+
+test "Test" {
+    const stsp = Command.collection.stsp;
+
+    var vvm: Vvm = undefined;
+    vvm.init();
+
+    vvm.memory[0] = stsp.code(); // STSP
+    vvm.registers.a.w[0] = 0x1231;
+    vvm.registers.sp = 0;
+    vvm.registers.pc = 0;
+    vvm.step();
+
+    try std.testing.expectEqual(0x1231, vvm.registers.sp);
+}
