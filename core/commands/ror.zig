24451101d070aa6bffcf562109682c0862699293
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 14:05:34 +0100

    WIP: refactoring command system - ROR

--- core/commands/ror.zig
+++ core/commands/ror.zig
@@ -1,28 +1,22 @@
 const std = @import("std");
 const Vvm = @import("../Vvm.zig");
-const commands = @import("../commands.zig");
-
-pub const descriptor = commands.Descriptor.init(0x59);
+const Command = @import("../Command.zig");
 
 pub fn handler(vvm: *Vvm) void {
-    const xa: u32 = (@as(u32, vvm.registers.x) << 16) + vvm.registers.a.w;
-    const xa_ror = (xa >> 1) + (xa << 31);
-    vvm.registers.a.w = @truncate(xa_ror);
-    vvm.registers.x = @intCast(xa_ror >> 16);
+    const xa = vvm.registers.a.dw;
+    vvm.registers.a.dw = (xa >> 1) + (xa << 31);
 }
 
 test "Test" {
+    const ror = Command.collection.ror;
     var vvm: Vvm = undefined;
 
-    vvm.memory[0] = @intCast(descriptor.base); // ROR
-    vvm.registers.a.w = 0xFED9;
-    vvm.registers.x = 0xBA98;
+    vvm.memory[0] = ror.code(0); // ROR
+    vvm.registers.a.w[0] = 0xFED9;
+    vvm.registers.a.w[1] = 0xBA98;
     vvm.registers.pc = 0;
     vvm.step();
 
     const result = @as(u32, @truncate((0xBA98FED9 >> 1) + 0x8000_0000));
-    const result_lsw: u16 = @truncate(result);
-    const result_hsw: u16 = result >> 16;
-    try std.testing.expectEqual(result_lsw, vvm.registers.a.w);
-    try std.testing.expectEqual(result_hsw, vvm.registers.x);
+    try std.testing.expectEqual(result, vvm.registers.a.dw);
 }
