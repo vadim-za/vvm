1a8f3162743f2a874745a269f3a6225f931b6f8d
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 11:19:31 +0100

    WIP: refactoring command system - ARWR

--- core/commands/arwr.zig
+++ core/commands/arwr.zig
@@ -1,13 +1,8 @@
 const std = @import("std");
 const Vvm = @import("../Vvm.zig");
-const commands = @import("../commands.zig");
+const Command = @import("../Command.zig");
 
-pub const descriptor = commands.Descriptor.initRange(
-    0x48,
-    4,
-);
-
-pub fn handler(comptime command_code: u8) commands.Handler {
+pub fn handler(comptime command_code: u8) Command.Handler {
     return struct {
         fn actualHandler(vvm: *Vvm) void {
             const index: u2 = command_code & 3;
@@ -17,12 +12,13 @@ pub fn handler(comptime command_code: u8) commands.Handler {
 }
 
 test "Test" {
+    const arwr = Command.collection.arwr;
     var vvm: Vvm = undefined;
 
-    for (0..descriptor.count) |n| {
+    for (0..arwr.variant_count) |n| {
         const value: u16 = 0x9110 + @as(u16, @intCast(n));
 
-        vvm.memory[0] = @intCast(descriptor.base + n); // LWR Wn
+        vvm.memory[0] = arwr.code(n); // ARWR Wn
         vvm.registers.gp.w[n] = value;
         vvm.registers.addr = 0;
         vvm.registers.pc = 0;
