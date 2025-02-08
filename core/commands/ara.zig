55e8561a9bffc5f93a275bfdd8ce74e7b2ecc6d4
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 14:29:53 +0100

    WIP: refactoring command system - ARA

---
+++ core/commands/ara.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const Vvm = @import("../Vvm.zig");
+const Command = @import("../Command.zig");
+
+pub fn handler(vvm: *Vvm) void {
+    vvm.registers.addr = vvm.registers.a.w[0];
+}
+
+test "Test" {
+    const ara = Command.collection.ara;
+    var vvm: Vvm = undefined;
+
+    vvm.memory[0] = ara.code(); // ARA
+    vvm.registers.a.w[0] = 0x9110;
+    vvm.registers.addr = 0;
+    vvm.registers.pc = 0;
+    vvm.step();
+
+    try std.testing.expectEqual(0x9110, vvm.registers.addr);
+}
