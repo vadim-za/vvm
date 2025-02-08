1756297962461ac217292d3e19cf19894db9a500
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 19:48:35 +0100

    Test CALL pushing across the ROM boundary

--- core/commands/call.zig
+++ core/commands/call.zig
@@ -12,6 +12,7 @@ test "Test" {
 
     var vvm: Vvm = undefined;
     vvm.init();
+    vvm.rom_addr = 0xF000;
 
     vvm.memory[0x1234] = call.code(); // CALL
     vvm.memory[0xEFFE] = 0;
@@ -25,4 +26,18 @@ test "Test" {
     try std.testing.expectEqual(0xEFFE, vvm.registers.sp);
     try std.testing.expectEqual(0x35, vvm.memory[0xEFFE]);
     try std.testing.expectEqual(0x12, vvm.memory[0xEFFF]);
+
+    // CALL pushing address across the ROM boundary
+    vvm.memory[0x1234] = call.code(); // CALL
+    vvm.memory[0xEFFF] = 0;
+    vvm.memory[0xF000] = 0;
+    vvm.registers.addr = 0x5678;
+    vvm.registers.sp = 0xF001;
+    vvm.registers.pc = 0x1234;
+    vvm.step();
+
+    try std.testing.expectEqual(0x5678, vvm.registers.pc);
+    try std.testing.expectEqual(0xEFFF, vvm.registers.sp);
+    try std.testing.expectEqual(0x35, vvm.memory[0xEFFF]);
+    try std.testing.expectEqual(0, vvm.memory[0xF000]); // not written
 }
