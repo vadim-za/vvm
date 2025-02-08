0c45571a1ca96bf42245158242a029a33e2fad44
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-08 19:33:42 +0100

    WIP: refactoring command system - JMP

---
+++ core/commands/jmp.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+const Vvm = @import("../Vvm.zig");
+const Command = @import("../Command.zig");
+
+pub fn handler(vvm: *Vvm) void {
+    vvm.registers.pc = vvm.registers.addr;
+}
+
+test "Test" {
+    const jmp = Command.collection.jmp;
+
+    var vvm: Vvm = undefined;
+    vvm.init();
+
+    vvm.memory[0] = jmp.code(); // JMP
+    vvm.registers.addr = 0x1002;
+    vvm.registers.pc = 0;
+    vvm.step();
+
+    try std.testing.expectEqual(0x1002, vvm.registers.pc);
+}
