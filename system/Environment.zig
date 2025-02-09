bcdbf514d1725dee4f560b09701acaa586c8f1b2
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-09 11:59:35 +0100

    Writing to console

--- system/Environment.zig
+++ system/Environment.zig
@@ -1,13 +1,16 @@
+const std = @import("std");
 const VvmCore = @import("VvmCore");
 const System = @import("System.zig");
 
 system: *System, // manually set by the owning System
 running: bool = false,
+output: std.fs.File.Writer,
 
 // System is not fully initialized yet at the time of the call
 pub fn init(self: *@This(), system: *System) void {
     self.* = .{
         .system = system,
+        .output = std.io.getStdOut().writer(),
     };
 }
 
@@ -21,8 +24,14 @@ pub fn envIn(ptr: ?*anyopaque, port: u8) u8 {
 
 pub fn envOut(ptr: ?*anyopaque, port: u8, value: u8) void {
     const self: *@This() = @alignCast(@ptrCast(ptr.?));
-    if (port == 0) {
-        if (value & 1 == 0)
-            self.running = false;
+    switch (port) {
+        0 => {
+            if (value & 1 == 0)
+                self.running = false;
+        },
+        1 => {
+            self.output.writeByte(value) catch {};
+        },
+        else => {},
     }
 }
--- system/System.zig
+++ system/System.zig
@@ -41,3 +41,21 @@ test "Test" {
     try std.testing.expect(system.run(10));
     try std.testing.expectEqual(3, core.registers.pc);
 }
+
+pub fn main() void {
+    var system: @This() = undefined;
+    system.init();
+
+    const code = [_]u8{
+        0x7A, 0x01, 0x00, // ARV 0x0001
+        0x62, 0x41, // LBV 'A'
+        0x55, // OUT
+        0x50, 0x5A, 0x55, // ZERO ; ARA ; OUT
+    };
+
+    const core = &system.core;
+    @memcpy(core.memory[0..code.len], &code);
+    core.registers.pc = 0;
+    if (!system.run(20))
+        std.debug.print("\nLooped\n", .{});
+}
