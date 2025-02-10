186432ba834495d2983473d16059174cf6064222
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-10 13:30:17 +0100

    Setup asm subproject

---
+++ asm/main.zig
@@ -0,0 +1,7 @@
+const std = @import("std");
+
+const source =
+    \\ lbv 0x10
+;
+
+pub fn main() void {}
--- build.zig
+++ build.zig
@@ -32,10 +32,28 @@ pub fn build(b: *std.Build) void {
     });
     system_tests.root_module.addImport("VvmCore", VvmCore);
 
+    const @"asm" = b.addExecutable(.{
+        .name = "vvmasm",
+        .root_source_file = b.path("asm/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    //@"asm".root_module.addImport("VvmCore", VvmCore);
+    b.installArtifact(@"asm");
+
+    const asm_tests = b.addTest(.{
+        .root_source_file = b.path("asm/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    //asm_tests.root_module.addImport("VvmCore", VvmCore);
+
     const run_core_tests = b.addRunArtifact(core_tests);
     const run_system_tests = b.addRunArtifact(system_tests);
+    const run_asm_tests = b.addRunArtifact(asm_tests);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_core_tests.step);
     test_step.dependOn(&run_system_tests.step);
+    test_step.dependOn(&run_asm_tests.step);
 }
