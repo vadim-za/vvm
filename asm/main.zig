7a14bf208f1e0fae0d65ce4edf59ee5d3a9cafd9
Vadim <153772150+vadim-za@users.noreply.github.com>
2025-02-10 14:48:51 +0100

    Setting up readers and writers for ASM

--- asm/main.zig
+++ asm/main.zig
@@ -11,8 +11,16 @@ pub fn main() !void {
     defer _ = gpa.deinit();
 
     var result: std.ArrayList(u8) = .init(alloc);
+    var in_stream = std.io.fixedBufferStream(source);
+
+    var in = in_stream.reader().any();
     var out = result.writer().any();
-    try out.print("ABCD", .{});
+    while (true) {
+        const byte = in.readByte() catch break;
+        try out.writeByte(byte);
+    }
+
+    //try out.print("ABCD", .{});
     std.debug.print("{x}\n", .{result.items});
     result.deinit();
 }
